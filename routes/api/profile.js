cda9b807ae14bb6203f347323673c39b044e7257
Zane Campbell <zanegitinit@gmail.com>
2019-06-07 15:03:17 -0700

    created delete profile and user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -143,4 +143,21 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user, & post
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+    res.json({ msg: 'User deleted' });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).json({ msg: 'Profile not found' });
+  }
+});
 module.exports = router;
